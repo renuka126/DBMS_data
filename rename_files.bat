6da0ba647fb973f03c229a1b03640d7717fb5b93
renuka123 <sunitapachghare@gmail.com>
2026-06-21 22:55:06 +0530

    Modified

---
+++ rename_files.bat
@@ -0,0 +1,17 @@
+@echo off
+git mv "Create table.txt" "create_table.sql"
+git mv "cursor.txt" "cursor.sql"
+git mv "Functons.txt" "functions.sql"
+git mv "GroupBy & Having.txt" "groupby_having.sql"
+git mv "Inserting values.txt" "inserting_values.sql"
+git mv "Joins.txt" "joins.sql"
+git mv "PL_SQL.txt" "pl_sql.sql"
+git mv "Rollback_transacation.txt" "rollback_transaction.sql"
+git mv "savepoint.txt" "savepoint.sql"
+git mv "Schedules.txt" "schedules.sql"
+git mv "Subquiries and Joins.txt" "subqueries_and_joins.sql"
+git mv "Transaction.txt" "transaction.sql"
+git mv "Triggers.txt" "triggers.sql"
+git mv "Views.txt" "views.sql"
+echo Done!
+pause
